6e87f8590d18fad75f65a3acaa3194b651cf7b0e
rupalimarwadi <rupali0103@gmail.com>
2024-03-10 13:02:04 +0530

    Chai aur react from 1-3 epi

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Test from './Test.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+    <App />
+    <Test /></>
+)
